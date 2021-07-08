4e1a3ee41b768b83094cd6814c6b3bd8f26be13b
Seneiya Cooke <seneiyacooke@gmail.com>
2021-07-08 15:41:01 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.body.remove(main);
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Seneiya is the champion!";
+newHeader.className = "victory"; 
